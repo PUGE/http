729fa76ef981bf5680441bd6e9b98a6af2277c09
PUGE <100284685@qq.com>
2017-10-08 20:14:39 +0800

    更新

--- bin/serve.js
+++ bin/serve.js
@@ -9,63 +9,32 @@ const args = require('args')
 const compress = require('micro-compress')
 const detect = require('detect-port')
 const { coroutine } = require('bluebird')
-const updateNotifier = require('update-notifier')
 const { red } = require('chalk')
 const nodeVersion = require('node-version')
 
-// Ours
+// Utilities
 const pkg = require('../package')
 const listening = require('../lib/listening')
 const serverHandler = require('../lib/server')
+const { options, minimist } = require('../lib/options')
 
 // Throw an error if node version is too low
 if (nodeVersion.major < 6) {
   console.error(
-    `${red('Error!')} Serve requires at least version 6 of Node. Please upgrade!`
+    `${red(
+      'Error!'
+    )} Serve requires at least version 6 of Node. Please upgrade!`
   )
   process.exit(1)
 }
 
-// Let user know if there's an update
-// This isn't important when deployed to production
-if (process.env.NODE_ENV !== 'production' && pkg.dist) {
-  updateNotifier({ pkg }).notify()
-}
+// Register the list of options
+args.options(options)
 
-args
-  .option('port', '监听端口', process.env.PORT || 5000)
-  .option(
-    'cache',
-    '浏览器文件缓存时间',
-    3600
-  )
-  .option('single', '仅使用一个index.html服务单页面应用程序')
-  .option('unzipped', '禁用GZIP压缩')
-  .option('ignore', '设置忽略的文件和目录')
-  .option('auth', '设置基本的认证')
-  .option(
-    'cors',
-    '是否允许跨域请求',
-    false
-  )
-  .option('silent', `不要在控制台打印任何信息`)
-  .option('no-clipboard', `不要将地址复制到粘贴板`, false)
-  .option('open', '服务器启动后打开本地地址', false)
-const flags = args.parse(process.argv, {
-  minimist: {
-    alias: {
-      a: 'auth',
-      C: 'cors',
-      S: 'silent',
-      s: 'single',
-      u: 'unzipped',
-      n: 'no-clipboard',
-      o: 'open'
-    },
-    boolean: ['auth', 'cors', 'silent', 'single', 'unzipped', 'no-clipboard', 'open']
-  }
-})
+// And initialize `args`
+const flags = args.parse(process.argv, { minimist })
 
+// Figure out the content directory
 const directory = args.sub[0]
 
 // Don't log anything to the console if silent mode is enabled
@@ -106,10 +75,13 @@ detect(port).then(open => {
     }
   }
 
-  server.listen(
-    port,
-    coroutine(function*() {
-      yield listening(server, current, inUse, flags.noClipboard !== true, flags.open)
-    })
-  )
+  const listenArgs = [
+    server,
+    current,
+    inUse,
+    flags.clipless !== true,
+    flags.open
+  ]
+
+  server.listen(port, listening.bind(this, ...listenArgs))
 })
